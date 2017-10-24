efff6598ebf78e0a9a4f91bb85f457c6b140f05a
AndreVictor60 <andre.victor60@free.fr>
2017-10-24 12:05:30 +0200

    Exercice 7

---
+++ exercice7.js
@@ -0,0 +1,39 @@
+function resultat(){
+  // Déclarer les variables & récuperer les deux nombres saisie par utilisateur
+  var size = document.getElementById('pointure').value;
+  var year = document.getElementById('annee').value;
+  //Vérification
+  if(isNaN(size) == true || isNaN(year) == true){
+        alert('Ceci n\est pas un chiffre');
+  }
+  else {
+  var  shoesize = size;
+  var  yearbith = year;
+    // Déclarer la variable résultat et on init a 0
+    var resultat = 0;
+    // Multiplier la pointure par 2
+    resultat = shoesize * 2;
+    // Ajouter 5 au résultat
+    resultat = resultat + 5;
+    // Multiplier le tout par 50
+    resultat = resultat * 50;
+    // Soustraire l’année de naissance
+    resultat = resultat - yearbith;
+    // Ajouter au tout 1766
+    resultat = resultat + 1766;
+    // Afficher le résultat
+    alert(resultat);
+
+  }
+
+}
+
+/* Demander à l’utilisateur sa pointure et son année de naissance. Créer une fonction qui fait les calculs suivants :
+
+    Multiplier la pointure par 2
+    Ajouter 5 au résultat
+    Multiplier le tout par 50
+    Soustraire l’année de naissance
+    Ajouter au tout 1766
+
+La fonction doit retourner le résultat. Tester avec votre date de naissance et votre pointure. Attention : n'utiliser une seule variable « resultat ». */
